2a7de42fa95b401bc13c6f7d1e8614e46cfc6cdf
41Guilherme <guilherme41@usp.br>
2021-08-04 18:22:17 -0300

    git

---
+++ calculadora1.py
@@ -0,0 +1,20 @@
+    
+class Calculadora:
+
+    def __init__(self, num1 , num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def soma(self):
+        return self.num1 + self.num2
+
+    def sub(self):
+        return self.num1 - self.num2
+
+    def mul(self):
+        return self.num1 * self.num2
+
+    def div(self):
+        return self.num1 / self.num2
+
+
